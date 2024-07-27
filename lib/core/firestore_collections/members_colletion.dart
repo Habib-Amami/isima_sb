b7c0d39e6a7e3df0d1266666e6acbe3dd2acba99
Habib-Amami <habibamami050@gmail.com>
2024-07-27 15:56:39 +0200

    static class for member collection (firestore)

---
+++ lib/core/firestore_collections/members_colletion.dart
@@ -0,0 +1,12 @@
+class MembersCollection {
+  static String collectionName = "members";
+  static const String id = 'id';
+  static const String photoURL = 'photoURL';
+  static const String username = 'username';
+  static const String studyLevel = 'studyLevel';
+  static const String phoneNumber = 'phoneNumber';
+  static const String email = 'email';
+  // ignore: constant_identifier_names
+  static const String FCMtoken = 'FCMtoken';
+  static const String role = 'role';
+}
